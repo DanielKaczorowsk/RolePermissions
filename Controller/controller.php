7db387f630ef5953feabf461494b5ca5f8527791
Daniel Kaczorowski <35191108+DanielKaczorowsk@users.noreply.github.com>
2023-05-29 19:44:36 +0200

    Add files via upload

--- Controller/controller.php
+++ Controller/controller.php
@@ -4,11 +4,21 @@ use Model;
 include "../"."SPL_autoload_register.php";
 class controller
 {
-	private $UserControl;
+	private $UserControl,$base;
 	public function __construct()
 	{
-		$UserControl = new Model\UserControl;
-		print_r($UserControl->loadRole('gosc'));
+		$this->UserControl = new Model\UserControl;
+		print_r($this->UserControl->loadUprawnienia('Edytor'));
+		$this->base = new Model\MyBasePDO;
+		$this->base->Connect();
+		$query = $this->base
+		->Select(['a.rola','b.uprawnienia'])
+		->From('role a')
+		->INNERJOIN('uprawnienie b')
+		->ON(['a.uprawnienia_id = b.id'])
+		->Where(['a.rola="gosc"'])
+		->getSql();
+		//print_r($query);
 	}
 }
 $controller = new controller;

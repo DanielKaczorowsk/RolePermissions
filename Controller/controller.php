31fc935e98dd49bb79ce3e30b516461359502efa
Daniel Kaczorowski <35191108+DanielKaczorowsk@users.noreply.github.com>
2023-05-31 18:58:13 +0200

    Add files via upload

--- Controller/controller.php
+++ Controller/controller.php
@@ -8,17 +8,17 @@ class controller
 	public function __construct()
 	{
 		$this->UserControl = new Model\UserControl;
-		print_r($this->UserControl->loadUprawnienia('Edytor'));
+		print_r($this->UserControl->findUserRoleId(1));
 		$this->base = new Model\MyBasePDO;
 		$this->base->Connect();
-		$query = $this->base
-		->Select(['a.rola','b.uprawnienia'])
+		/*$query = $this->base
+		->Select(['a.rola','b.permission','u.name'])
 		->From('role a')
-		->INNERJOIN('uprawnienie b')
-		->ON(['a.uprawnienia_id = b.id'])
-		->Where(['a.rola="gosc"'])
+		->INNERJOIN(['permission b','users u'],['a.uprawnienia_id = b.id','a.users_id = u.id'])
+		->Where(['a.rola="Admin"'])
 		->getSql();
-		//print_r($query);
+		print_r($query);
+		*/
 	}
 }
 $controller = new controller;
